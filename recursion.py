f5182dd6c043f809589b8ffa850ece719baa2033
Qwerty <mail@mail.com>
2025-06-01 09:12:16 +0500

    Adding recursion example

--- recursion.py
+++ recursion.py
@@ -4,3 +4,16 @@ def fac(number):
     if number == 0:
         return 1
     return number * fac(number - 1)
+
+
+'''
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+'''
+def digital_root(n):
+    if n // 10 == 0:
+        return n
+    else:
+        return digital_root(digital_root(n % 10) + digital_root(n // 10))
