1ea8e81f9c989a71f38571f28562f35f10e7c4b5
adilkhanekt <59197619+adilkhanekt@users.noreply.github.com>
2025-08-13 11:07:06 +0500

    Update search.py

--- search.py
+++ search.py
@@ -43,8 +43,27 @@ while low <= high:
         high = mid - 1
     else:
         low = mid + 1
-
-
+        
 _bin_end = time.time()
 print(f"Iterations count: {_bin_iter_count}")
 print(f"Execution time: {_bin_end - _bin_start} seconds")
+
+
+
+print("--------- Binary search in recursion (less optimal) ---------")
+
+def mysearch(array, num, depth):
+    print(array)
+    arr_len = len(array)
+    mid = arr_len // 2
+    depth += 1
+    if num == array[mid]:
+        return True, depth
+    elif num > array[mid]:
+        return mysearch(array[mid+1:arr_len], num, depth)
+    else:
+        return mysearch(array[0:mid], num, depth)
+
+search_list = list(range(100))
+output = mysearch(search_list, 3, 0)
+print(output)
